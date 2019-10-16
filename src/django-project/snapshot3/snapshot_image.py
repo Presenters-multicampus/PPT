55580237070111a0ea690ec05a9c069af8d2cb02
joyoon729 <joyoon729@gmail.com>
2019-10-16 09:28:30 +0900

    191014 | Applying face sentiment, scoring algorithm(prototype) on django code (#8)
    * 191014 | add 'get face sentiment' feature
    
    * 191014 | scoring algorithm prototype

--- src/django-project/snapshot3/snapshot_image.py
+++ src/django-project/snapshot3/snapshot_image.py
@@ -2,13 +2,13 @@ import cv2
 import numpy as np
 import os, shutil
 from django.conf import settings
-from .coco import getSkeleton
+from .processing import *
 
 def snapshot(url, SEC=3):
     filename = os.path.basename(url)
-    url = os.path.join(settings.MEDIA_ROOT, filename)
+    url = os.path.join(settings.MEDIA_ROOT, filename)  # video 파일 가져올 경로
     cap = cv2.VideoCapture(url)
-    save_dir = os.path.join(settings.SNAPS_DIR, filename)
+    save_dir = os.path.join(settings.SNAPS_DIR, filename)  # snapshots 저장할 경로
 
     if os.path.exists(save_dir):  # if filename is duplicated, remove that directory
         shutil.rmtree(save_dir)
@@ -19,22 +19,50 @@ def snapshot(url, SEC=3):
     except OSError:
         print('Error: Creating directory of data')
 
-    fps = round(cap.get(cv2.CAP_PROP_FPS))
+    fps = round(cap.get(cv2.CAP_PROP_FPS))  # video 의 fps 값 얻기
     ret, frame = cap.read()
+    
+    
+
     cf = 0  # Current Frame
+    emo_list = []  # emotion 들 저장하는 list
+    all_score = []  # 각 snapshot 의 점수 저장하는 list
+    # video -> snapshot 프레임 한장 한장 순회하는 반복문
     while(ret):
         # capture frame-by-frame
         ret, frame = cap.read()
 
         # saves image of the current frame in jpg file
         if cf%(fps*SEC) == 0:
-            frame, _, _ = getSkeleton(frame)
-            name = os.path.join(save_dir, f'{str(cf)}.jpg')
+            ## getSkeleton funcion call in 'preprocessing.py'
+            img_skeleton, img_with_dot, points_with_num, emotion = getSkeleton(frame)
+
+            current_score = scoring(points_with_num, emotion)  # scoring function call
+
+            name = os.path.join(save_dir, f'{str(cf)}.jpg')  # ex) '100.jpg'
             print(f'Creating... {name}')
-            cv2.imwrite(name, frame)
+            cv2.imwrite(name, img_skeleton)
+            emo_list.append(emotion)
+            all_score.append(current_score)
 
         cf += 1
 
     cap.release()
+    emo_path = os.path.join(save_dir, 'snap_info.txt')
+    print(emo_list)
+
+    # snap_info.txt 에 snapshot 순서대로 write 하기
+    # snap_info.txt --- example
+    # Fear,10
+    # None,5
+    # Surprise,10
+    # Happy,-5
+    # ----------------
+    with open(emo_path, 'w') as f:
+        for e, s in zip(emo_list, all_score):
+            f.write(e + ',' + s)  # 나중에 split(',') 으로 나누기 [emotion, score]
+            f.write('\n')
+
+
     print('\n  Done. \n')
     return save_dir
--- src/django-project/snapshot3/views.py
+++ src/django-project/snapshot3/views.py
@@ -7,12 +7,15 @@ import os, shutil
 from .tasks import snapshot_celery
 
 # Create your views here.
-SEC = 6  # 몇초마다 snapshot 할지 결정
+SEC = 30  # 몇초마다 snapshot 할지 결정
 
+# 홈 페이지 렌더링
 def home(request):
     files = FileModel.objects.all()
     return render(request, 'home.html', {'files':files})
 
+# 새 video 추가 및 'snapshot' 함수 실행
+# split, object inferencing
 def video_new(request):
     if request.method == 'POST':
         file = FileModel()
@@ -28,12 +31,13 @@ def video_new(request):
         form = FileForm()
     return render(request, 'video_new.html', {'form':form})
 
+# 홈페이지에서 제목 클릭했을 시 비디오 첫화면 렌더링
 def video_detail(request, pk):
     video = get_object_or_404(FileModel, pk=pk)
     videoname = os.path.basename(video.file.url)  # 선택된 video name
     snaps_dir = os.path.join(settings.SNAPS_DIR, videoname)  # 선택된 video들의 snapshot 들이 저장되어 있는 디렉토리 경로
     snapshots = os.listdir(snaps_dir)  # 전체 snapshot 리스트
-    snapshots = sorted(snapshots, key=lambda x: int(x.split('.')[0]))
+    snapshots = sorted(snapshots[:-1], key=lambda x: int(x.split('.')[0]))
     snapshots = list(map(lambda x: os.path.join('snapshots', videoname, x), snapshots))  # 숫자 기준으로 정렬
 
     # [[index, snapshot], [index, snapshot], ... ] 형태의 리스트로 변환
@@ -48,7 +52,7 @@ def video_snapshot(request, pk, idx):
     videoname = os.path.basename(video.file.url)
     snaps_dir = os.path.join(settings.SNAPS_DIR, videoname)
     snapshots = os.listdir(snaps_dir)
-    snapshots = sorted(snapshots, key=lambda x: int(x.split('.')[0]))
+    snapshots = sorted(snapshots[:-1], key=lambda x: int(x.split('.')[0]))
     snapshots = list(map(lambda x: os.path.join('snapshots', videoname, x), snapshots))
     
     dict_snapshots = {index:snapshot for index, snapshot in enumerate(snapshots)}
@@ -56,10 +60,26 @@ def video_snapshot(request, pk, idx):
     # [[index, snapshot], [index, snapshot], ... ] 형태의 리스트로 변환
     snapshots = [[idx, snapshot] for idx, snapshot in enumerate(snapshots)]
 
-    snapshot = dict_snapshots[idx]  # 선택된 스냅샷
+    # read snap_info.txt
+    # snapshot 함수에서 snap_info.txt 작성하고, 여기서 불러옵니다.
+    emo_list = []
+    sco_list = []
+    info_path = os.path.join(snaps_dir, 'snap_info.txt')
+    with open(info_path, 'r') as f:
+        while True:
+            line = f.readline()
+            if not line:
+                break
+            emo = line.split(',')[0].strip()
+            sco = line.split(',')[1].strip()
+            emo_list.append(emo)
+            sco_list.append(sco)
+
+    snapshot = [dict_snapshots[idx], emo_list[idx]]  # [선택된 스냅샷, 그 스냅샷의 emotion]
     sec = idx*SEC
     return render(request, 'video_detail.html', {'video':video, 'snapshots':snapshots, 'snap':snapshot, 'sec':sec})
 
+# 홈페이지의 'clear db' 링크 버튼 클릭시 실행
 # db 내 FileModel objects
 # media/ 내 video 파일
 # snapshot3/static/snapshots/ 내 snapshot 파일들
